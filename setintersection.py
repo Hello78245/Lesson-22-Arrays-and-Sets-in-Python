18b04f82a297a0142b450636447699b02998815e
Hello78245 <shinimagesh10@gmail.com>
2026-04-06 20:12:53 -0700

    setintersection

---
+++ setintersection.py
@@ -0,0 +1,8 @@
+setx={"red","crimson"}
+sety={"crimson","charteuse"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of 2 said sets")
+setz=setx.intersection(sety)
+print(setz)
