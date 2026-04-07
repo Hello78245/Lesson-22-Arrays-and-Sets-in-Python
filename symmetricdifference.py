18b64168cc2c6190105e0d0c5721f71b157ef12a
Hello78245 <shinimagesh10@gmail.com>
2026-04-06 20:26:29 -0700

    symmetricdifference

---
+++ symmetricdifference.py
@@ -0,0 +1,8 @@
+setx={"Vermillion","Charteuse"}
+sety={"Vermillion","Blue"}
+print("Original Set elements")
+print(setx)
+print(sety)
+print("\n Symmetric Difference of the 2 said sets")
+setz=setx.symmetric_difference(sety)
+print(setz)
